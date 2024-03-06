96d7881844eaa8936069e80200b392ccab8b7fac
AMAAN SARFARAZ <sarfarazmohammad369@gmail.com>
2024-03-07 00:15:28 +0530

    todo using redux tool kit

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodos from './components/AddTodos'
+import Simpletodos from './components/Simpletodos'
+
+function App() {
+
+  return (
+    <>
+    <h1>redux toolkit</h1>
+    <AddTodos/>
+    <Simpletodos/>
+    </>
+  )
+}
+
+export default App
---
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    todos: [{id: 1, text: "Hello"}]
+}
+
+export const todoSlice = createSlice({
+    name: "todos",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            state.todos.push({id: nanoid(), text: action.payload})
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id!== action.payload)
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id? action.payload : todo)
+        },
+        toggleComplete: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload? {...todo, completed:!todo.completed} : todo)
+        }
+    }
+})
+
+export const { addTodo, removeTodo, updateTodo, toggleComplete } = todoSlice.actions
+
+export default todoSlice.reducer
